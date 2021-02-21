54c86169253d15c944f223e9a8d65ad7df6b052c
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2021-02-21 10:47:32 +0900

    Change to install Go and binaries commands

--- gce.tf
+++ gce.tf
@@ -36,6 +36,16 @@ provider "google" {
     region = var.region
 }
 
+variable "gce_ssh_user" {
+    type = string
+    description = "username for SSH"
+    default = "demo"
+}
+variable "gce_ssh_pub_key_file" {
+    type = string
+    description = "path the public key file"
+    default = "~/.ssh/gcp_terraform.pub"
+}
 resource "google_compute_firewall" "dev_http" {
     name = "dev-http"
     network = "default"
@@ -49,8 +59,8 @@ resource "google_compute_firewall" "dev_http" {
     target_tags = ["tracability-sample"]
 }
 
-resource "google_compute_instance" "debian10_dev" {
-    name = "dev"
+resource "google_compute_instance" "debian10" {
+    name = "debian10"
     machine_type = "e2-standard-4"
     zone = var.zone
     tags = ["dev-env"]
@@ -76,6 +86,11 @@ resource "google_compute_instance" "debian10_dev" {
         scopes = ["compute-rw", "logging-write", "monitoring"]
     }
 
+    metadata = {
+        block-project-ssh-keys = "true"
+        ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+    }
+
     provisioner "local-exec" {
         working_dir = "./ansible/"
         command = <<EOL
@@ -84,8 +99,8 @@ resource "google_compute_instance" "debian10_dev" {
     }
 }
 
-resource "google_compute_instance" "ubuntu2004_dev" {
-    name = "dev"
+resource "google_compute_instance" "ubuntu2004" {
+    name = "ubuntu2004"
     machine_type = "e2-standard-4"
     zone = var.zone
     tags = ["dev-env"]
@@ -111,6 +126,11 @@ resource "google_compute_instance" "ubuntu2004_dev" {
         scopes = ["compute-rw", "logging-write", "monitoring"]
     }
 
+    metadata = {
+        block-project-ssh-keys = "true"
+        ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+    }
+
     provisioner "local-exec" {
         working_dir = "./ansible/"
         command = <<EOL
@@ -120,7 +140,7 @@ resource "google_compute_instance" "ubuntu2004_dev" {
 }
 
 resource "google_compute_instance" "arch_dev" {
-    name = "dev"
+    name = "arch"
     machine_type = "e2-standard-2"
     zone = var.zone
     tags = ["dev-env"]
@@ -144,6 +164,11 @@ resource "google_compute_instance" "arch_dev" {
 
     service_account {
         scopes = ["compute-rw", "logging-write", "monitoring"]
+    }
+
+    metadata = {
+        block-project-ssh-keys = "true"
+        ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
     }
 
     provisioner "local-exec" {
